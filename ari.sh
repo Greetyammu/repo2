2f75e6594168f095c074514ed938f1e0ddf19eba
Greetyammu <greetygreetings1996@gmail.com>
2019-03-19 20:25:58 +0530

    first commit

---
+++ ari.sh
@@ -0,0 +1,8 @@
+echo "Enter two numbers"
+read a b 
+echo "$a+$b="`expr $a + $b`
+echo "$a-$b="`expr $a - $b`
+echo "$a*$b="`expr $a \* $b`
+echo "$a/$b="`expr $a / $b`
+echo "$a%$b="`expr $a % $b`
+
